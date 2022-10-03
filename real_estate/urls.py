3331ccfa2ad78f27c4df0dc702803f785bd630ba
bjamyl <alhassanjamil0@gmail.com>
2022-10-03 03:04:25 +0000

    reset configs

--- real_estate/urls.py
+++ real_estate/urls.py
@@ -17,9 +17,5 @@ urlpatterns = [
     path('api/', include('enquiries.api.urls')),
     path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

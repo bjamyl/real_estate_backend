af02bad556fd08f0b291fa535af375ffd1d3797c
bjamyl <alhassanjamil0@gmail.com>
2022-12-15 04:18:32 +0000

    minor change for deployment

--- listings/api/views.py
+++ listings/api/views.py
@@ -7,6 +7,7 @@ from rest_framework import generics
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 
+
 class ListingsView(APIView):
     def get(self,):
         listings = Listing.objects.all()
